8e0e210ba5960033acd81d56a3fbb493ef89f2ed
francesco.megna97 <francesco.megna97@gmail.com>
2019-01-19 16:40:37 +0100

    init with some components and test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
-}
+export const Every = ({ on = [], condition, children }) =>
+  on.every(condition) ? children : null;
+
+export const Filter = ({ on = [], condition, children }) =>
+  on.map(v => (condition(v) ? children : null));
+
+export const First = ({ on = [] }) =>
+  on[0];
+
+export const If = ({ condition, elze, children }) =>
+  (condition ? children : elze);
+
+export const Last = ({ on = [] }) =>
+  on[on.length - 1]
+
+export const Map = ({ on = [], children }) =>
+  on.map(children)
+
+export const Some = ({ on = [], condition, children }) =>
+  on.some(condition) ? children : null
+
+export const Includes = ({ on = [], searchElement, fromIndex = 0, children }) =>
+  on.includes(searchElement, fromIndex) ? children : null
+
+export const Sort = ({ on = [], children }) =>
+  on.sort().map(children)
+
+export const Times = ({ n = 0, children }) =>
+  [...Array(n)].map((v, i) => children(i));
+
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,10 +1,21 @@
 import expect from 'expect'
 import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
 
-import Component from 'src/'
+import {
+  Every,
+  Filter,
+  First,
+  If,
+  Last,
+  Map,
+  Some,
+  Includes,
+  Sort,
+  Times
+} from "../src"
 
-describe('Component', () => {
+describe('Components', () => {
   let node
 
   beforeEach(() => {
@@ -15,9 +26,125 @@ describe('Component', () => {
     unmountComponentAtNode(node)
   })
 
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
+  it('render by every', () => {
+    render((
+      (
+        <Every
+          on={[1, 4, 9]}
+          condition={v => v <= 10}
+        >
+          every
+        </Every>
+      )
+    ), node, () => {
+      expect(node.innerHTML).toContain("every")
+    })
+  })
+
+  it("render by Filter", () => {
+    // render((
+    //   <Filter
+    //     on={["banana", "kiwi"]}
+    //     condition={v => v === "kiwi"}
+    //   >
+    //     {v => v}
+    //   </Filter>
+    // ), node, () => {
+    //   console.log(node.innerHTML)
+    //   expect(node.innerHTML).toContain("kiwi")
+    // })
+  })
+
+  it("render by If", () => {
+    render((
+      <If condition={10 === 10}>
+        by if
+      </If>
+    ), node, () => {
+      expect(node.innerHTML).toContain("by if")
+    })
+  })
+
+  it("render by else from If", () => {
+    render((
+      <If
+        condition={10 === 1}
+        elze={"fallback"}
+      >
+        by if
+      </If>
+    ), node, () => {
+      expect(node.innerHTML).toBe("fallback")
+    })
+  })
+
+  it("render by First", () => {
+    render((
+      <First
+        on={["apple", "ananas"]}
+      >
+        {v => v}
+      </First>
+    ), node, () => {
+      expect(node.innerHTML).toContain("apple")
+    })
+  })
+
+  it("render by Last", () => {
+    render((
+      <Last
+        on={["apple", "ananas"]}
+      >
+        {v => v}
+      </Last>
+    ), node, () => {
+      expect(node.innerHTML).toContain("ananas")
+    })
+  })
+
+  it("render by Map", () => {
+    render((
+      <Map
+        on={["lemon", "watermelon"]}
+      >
+        {(v, i) => ` ${i}: ${v}`}
+      </Map>
+    ), node, () => {
+      expect(node.innerHTML).toContain("0: lemon 1: watermelon")
+    })
+  })
+
+  it("render by Some", () => {
+    render((
+      <Some
+        on={["apricot", "avocado"]}
+        condition={v => v === "apricot"}
+      >
+        "avocado"
+      </Some>
+    ), node, () => {
+      expect(node.innerHTML).toContain("avocado")
+    })
+  })
+
+  it("render by Includes", () => {
+    render((
+      <Includes
+        on={["grape", "fig"]}
+        searchElement="grape"
+      >
+        grape
+      </Includes>
+    ), node, () => {
+      expect(node.innerHTML).toBe("grape")
+    })
+  })
+
+  it("render by Times", () => {
+    render((
+      <Times n={2}>{i => "gg"}</Times>
+    ), node, () => {
+      expect(node.innerHTML).toBe("heyheyhey")
     })
   })
 })
